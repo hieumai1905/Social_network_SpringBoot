5369d690037529d186063a854d0be35de32985f1
tun tun <hieumai1905it@gmail.com>
2024-01-08 21:24:20 +0700

    create project

---
+++ src/test/java/com/example/socialnetworkspringboot/SocialNetworkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialnetworkspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
